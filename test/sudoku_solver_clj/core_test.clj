3e1c68cbfc3549a3ea0c052f9a3b06f7a4546c59
heruku <uku.taht@gmail.com>
2014-05-13 18:53:44 +0100

    initial commit

---
+++ test/sudoku_solver_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns sudoku-solver-clj.core-test
+  (:require [clojure.test :refer :all]
+            [sudoku-solver-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
