7b69e17fecb45edac0faa38e2546c862e2796fc0
heruku <uku.taht@gmail.com>
2014-05-13 19:59:36 +0100

    convert input string to vector

--- src/sudoku_solver_clj/core.clj
+++ src/sudoku_solver_clj/core.clj
@@ -1,7 +1,22 @@
 (ns sudoku-solver-clj.core
   (:gen-class))
 
+(defn solve [board]
+  board
+  )
+
+(defn explode-to-ints [string]
+  (map #(Character/digit %1 10) string)
+  )
+
+(defn str-to-grid [string board-size]
+  "Converts the input string to a 2-dimensional vector"
+  (loop [string string
+         result []]
+    (if (empty? string)
+      (reverse result)
+      (recur (drop board-size string) (cons (explode-to-ints (take board-size string)) result)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+    (solve (str-to-grid (first args) (Math/sqrt (count (first args))))))
--- test/sudoku_solver_clj/core_test.clj
+++ test/sudoku_solver_clj/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [sudoku-solver-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parsing
+  (testing "Creates a 2 dimensional vector from input string"
+    (is (= [[1 0 5 8 0 2 0 0 0] 
+            [0 9 0 0 7 6 4 0 5]
+            [2 0 0 4 0 0 8 1 9]
+            [0 1 9 0 0 7 3 0 6]
+            [7 6 2 0 8 3 0 9 0]
+            [0 0 0 0 6 1 0 5 0]
+            [0 0 7 6 0 0 0 3 0]
+            [4 3 0 0 2 0 5 0 1]
+            [6 0 0 3 0 8 9 0 0]]
+         (str-to-grid "105802000090076405200400819019007306762083090000061050007600030430020501600308900" 9)))))
